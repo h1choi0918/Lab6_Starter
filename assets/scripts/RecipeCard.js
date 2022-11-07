8f4eba036e5d2da9a9908f9c7646335fee8514dc
Raon Choi <h1choi@ucsd.edu>
2022-11-06 17:07:09 -0800

    Update RecipeCard.js

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -10,9 +10,9 @@ class RecipeCard extends HTMLElement {
     // A1. TODO - Attach the shadow DOM to this Web Component (leave the mode open)
     this.attachShadow({mode:"open"});
     // A2. TODO - Create an <article> element - This will hold our markup once our data is set
-    const art = document.createElement("article");
+    let art = document.createElement("article");
     // A3. TODO - Create a style element - This will hold all of the styles for the Web Component
-    const style = document.createElement("style");
+    let style = document.createElement("style");
     // A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made
     style.textContent = `
       * {
@@ -120,13 +120,30 @@ class RecipeCard extends HTMLElement {
     if (!data) return;
 
     // A6. TODO - Select the <article> we added to the Shadow DOM in the constructor
-    
+    let article = this.shadowRoot.querySelector("article");
     // A7. TODO - Set the contents of the <article> with the <article> template given in
     //           cardTemplate.html and the data passed in (You should only have one <article>,
     //           do not nest an <article> inside another <article>). You should use Template
     //           literals (tempalte strings) and element.innerHTML for this.
+    article.innerHTML = `
+      <img src="https://link-to-article.com/recipe-thumbnail.jpg" alt="Recipe Title">
+      <p class="title">
+        <a href="https://link-to-article.com">Title</a>
+      </p>
+      <p class="organization">The Chef's Organization</p>
+      <div class="rating">
+        <span>5</span>
+        <img src="/assets/images/icons/5-star.svg" alt="5 stars">
+        <span>(500)</span>
+      </div>
+      <time>50 min</time>
+      <p class="ingredients">
+        Comma, Separated, List, of, Ingredients
+      </p>
+    `;
   }
 }
 
 // A8. TODO - Define the Class as a customElement so that you can create
 //           'recipe-card' elements
+customElements.define("recipe-card", RecipeCard);
